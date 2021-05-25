b39e9eb672bd4dd4f8b2c048d042510b62245fc8
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-25 12:54:47 +0530

    Routes Created!

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the Contact Keeper API!' })
 })
 
+app.get('/api/users', require('./routes/users'))
+app.get('/api/auth', require('./routes/auth'))
+app.get('/api/contacts', require('./routes/contacts'))
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on Port=>${PORT}`))
